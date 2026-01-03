a13df11a12cf47631cadb0503a2d26656291564c
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-01-03 14:18:21 +0530

    Done with removing the duplicates of an sorted array

---
+++ Arrays/RemoveDupSorted.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.util.HashSet;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 0;
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.contains(num)) {
+                seen.add(num);
+                nums[index] = num;
+                index++;
+            }
+        }
+        return index;
+    }
+}
+
+public class RemoveDupSorted {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = { 1, 1, 1, 2, 2, 2, 2, 3, 3, 3 };
+        int k = sol.removeDuplicates(nums);
+
+        System.out.println("k :" + k);
+        System.out.println("Array after removing duplicates");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
