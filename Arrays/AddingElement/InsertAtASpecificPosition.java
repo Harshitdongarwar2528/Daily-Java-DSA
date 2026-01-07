9fc35ce3037fd7f23a4fa744a6461dee3aaf8795
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-01-07 12:02:21 +0530

    Today i have done inserting an element at a specfic position in an array

---
+++ Arrays/AddingElement/InsertAtASpecificPosition.java
@@ -0,0 +1,26 @@
+package Arrays.AddingElement;
+class SOL {
+    public static int[] InsertAtAPos(int [] arr, int pos , int x){
+        int [] newArr = new int [arr.length+1];
+        for(int i = 0 ; i < arr.length ; i ++){
+            newArr[i] = arr[i];
+        }
+        newArr[pos] = x;
+
+        for(int i = pos ; i < arr.length ; i++){
+            newArr[i+1]=arr[i];
+        }
+        return newArr;
+    }
+}
+public class InsertAtASpecificPosition{
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,6,7};
+        int x = 5 , pos = 4;
+        arr = SOL.InsertAtAPos(arr, pos, x);
+        for(int num : arr){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+    }
+}
