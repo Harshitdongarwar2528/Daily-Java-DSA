0afe32eefb03aa96dd4617d75eb050a9dc510832
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-02-28 20:53:49 +0530

    Added the problem statement to stars and hashes

--- Logic_Problems/StarsAndHashes.java
+++ Logic_Problems/StarsAndHashes.java
@@ -1,19 +1,32 @@
+// Problem Statement – Given a string S(input consisting) of ‘*’ and ‘#’. The length of the string is variable. The task is to find the minimum number of ‘*’ or ‘#’ to make it a valid string. The string is considered valid if the number of ‘*’ and ‘#’ are equal. The ‘*’ and ‘#’ can be at any position in the string.
+// Note : The output will be a positive or negative integer based on number of ‘*’ and ‘#’ in the input string.
+
+// (*>#): positive integer
+// (#>*): negative integer
+// (#=*): 0
+// Example 1:
+// Input 1:
+
+// ###***   -> Value of S
+// Output :   → number of * and # are equal
+
+package Logic_Problems;
 
 import java.util.*;
+
 public class StarsAndHashes {
-    public static void main (String [] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        int star = 0 , hash = 0;
-        for(int i  = 0 ; i < str.length() ; i++){
-            if(str.charAt(i) == ('*')){
+        int star = 0, hash = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ('*')) {
                 star++;
-            } else if ( str.charAt(i)== ('#')){
+            } else if (str.charAt(i) == ('#')) {
                 hash++;
             }
         }
-        System.out.println(star-hash);
+        System.out.println(star - hash);
         sc.close();
     }
 }
-
