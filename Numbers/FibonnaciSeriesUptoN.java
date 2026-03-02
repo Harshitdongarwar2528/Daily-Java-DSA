85d1c7c023b16606d0fe097f43dec71904a78f73
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-03-02 11:40:18 +0530

    Done with the Fibonacci Series upto n problem

---
+++ Numbers/FibonnaciSeriesUptoN.java
@@ -0,0 +1,29 @@
+package Numbers;
+
+public class FibonnaciSeriesUptoN {
+    public static void main(String[] args) {
+        int n = 5;
+
+        if (n <= 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        int secondLast = 0;
+        int last = 1;
+
+        System.out.println("The Fibonacci Series upto " + n + " terms:");
+
+        if (n >= 1)
+            System.out.print(secondLast + " ");
+        if (n >= 2)
+            System.out.print(last + " ");
+
+        for (int i = 3; i <= n; i++) {
+            int cur = secondLast + last;
+            System.out.print(cur + " ");
+            secondLast = last;
+            last = cur;
+        }
+    }
+}
