6336c5efb700938abd260f99c4a330835eb96f8e
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-03-03 18:30:23 +0530

    done with the problem of Ascii value

---
+++ Strings/ASCIIValue.java
@@ -0,0 +1,15 @@
+package Strings;
+
+class Ascii {
+    void value() {
+        char c = 'B';
+        System.out.println("The ASCII Value of " + c + " is " + (int) c);
+    }
+}
+
+public class ASCIIValue {
+    public static void main(String[] args) {
+        Ascii p1 = new Ascii();
+        p1.value();
+    }
+}
