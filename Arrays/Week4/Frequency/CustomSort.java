8df46528a59022a42582a5bee8f7e064f7191299
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-03-03 18:47:41 +0530

    Added the classes in the file to prevent error

--- Arrays/Week4/Frequency/CustomSort.java
+++ Arrays/Week4/Frequency/CustomSort.java
@@ -1,3 +1,4 @@
+package Arrays.Week4.Frequency;
 import java.util.*;
 public class CustomSort {
     public static void main(String[] args) {
--- Arrays/Week4/Frequency/FrequnecySort.java
+++ Arrays/Week4/Frequency/FrequnecySort.java
@@ -1,3 +1,5 @@
+package Arrays.Week4.Frequency;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
