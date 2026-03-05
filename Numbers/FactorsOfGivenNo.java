6c711c761d3d4ab4d69f996127e184a4d5dcb7c9
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-03-05 20:44:35 +0530

    Done with the Factors Problem

---
+++ Numbers/FactorsOfGivenNo.java
@@ -0,0 +1,13 @@
+package Numbers;
+
+public class FactorsOfGivenNo {
+    public static void main(String[] args) {
+        int n = 9;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.println("The factors are " + i);
+            }
+
+        }
+    }
+}
