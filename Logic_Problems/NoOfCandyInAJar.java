3fdabc1aabaa0ded69f6b6ef1c1f38ce5d5a26ce
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-03-07 10:37:23 +0530

    Done with the Numbers Of Candies In a Jar problem

---
+++ Logic_Problems/NoOfCandyInAJar.java
@@ -0,0 +1,42 @@
+// There is a JAR full of candies for sale at a mall counter. JAR has the capacity N, that is JAR can contain maximum N candies when JAR is full. At any point of time. JAR can have M number of Candies where M<=N. Candies are served to the customers. JAR is never remain empty as when last k candies are left. JAR if refilled with new candies in such a way that JAR get full.
+// Write a code to implement above scenario. Display JAR at counter with available number of candies. Input should be the number of candies one customer can order at point of time. Update the JAR after each purchase and display JAR at Counter.
+
+// Output should give number of Candies sold and updated number of Candies in JAR.
+
+// If Input is more than candies in JAR, return: “INVALID INPUT”
+// Given,
+// N=10, where N is NUMBER OF CANDIES AVAILABLE
+// K =< 5, where k is number of minimum candies that must be inside JAR ever.
+// Example 1:(N = 10, k =< 5)
+
+// Input Value
+// 3
+// Output Value
+// NUMBER OF CANDIES SOLD : 3
+// NUMBER OF CANDIES LEFT : 7
+
+package Logic_Problems;
+import java.util.Scanner;
+
+public class NoOfCandyInAJar {
+    public static void main(String[] args) {
+        int n = 10;
+        int k = 5;
+        int candy;
+        Scanner sc = new Scanner(System.in);
+        candy = sc.nextInt();
+
+        if (candy > n || n - candy < k) {
+            System.out.println("Invalid Input");
+            System.out.println("Number of candies left : " + n);
+        } else {
+            n = n - candy;
+            System.out.println("The Number of Candies Sold : " + candy);
+            System.out.println("The Number of Canides Remaning : " + n);
+
+        }
+
+        sc.close();
+
+    }
+}
