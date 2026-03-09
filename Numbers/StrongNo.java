0ccec5cc60a58c24ac388932665804360e6bffdb
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-03-09 13:19:18 +0530

    Done with the strong numbers

---
+++ Numbers/StrongNo.java
@@ -0,0 +1,32 @@
+package Numbers;
+
+public class StrongNo {
+    public static int Factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+
+        }
+        return fact;
+    }
+
+    public static int Strong_No(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Factorial(digit);
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int number = 125;
+        int ans = Strong_No(number);
+        if (ans == number && number != 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
