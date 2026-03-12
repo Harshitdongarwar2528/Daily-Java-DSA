fa1965a6364699e4a9bfabc09b6cb3690c46f38e
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-03-12 13:43:01 +0530

    Done with the Harshad Number

---
+++ Numbers/HarshadNo.java
@@ -0,0 +1,26 @@
+package Numbers;
+import java.util.*;
+
+public class HarshadNo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int original = n;
+        int sum = 0;
+
+        while(n > 0){
+            int digit = n % 10;
+            sum += digit;
+            n = n / 10;
+        }
+
+        if(original % sum == 0){
+            System.out.println("It is a Harshad Number");
+        }else{
+            System.out.println("Not a Harshad Number");
+        }
+        sc.close();
+    }
+}
