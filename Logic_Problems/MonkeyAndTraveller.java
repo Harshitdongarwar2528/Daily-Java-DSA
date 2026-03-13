0f527fc19ba46b3586623bf3cc8733f82b7360cb
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-03-13 10:32:15 +0530

    Done with the Monkey Problems

---
+++ Logic_Problems/MonkeyAndTraveller.java
@@ -0,0 +1,39 @@
+package Logic_Problems;
+
+import java.util.*;
+
+public class MonkeyAndTraveller {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int j = sc.nextInt();
+        int m = sc.nextInt();
+        int p = sc.nextInt();
+
+        int ateBanana = 0, atePeanut = 0;
+
+        if (n <= 0 || k <= 0 || j <= 0 || m < 0 || p < 0) {
+            System.out.println("INVALID INPUT");
+        } else {
+
+            ateBanana = m / k;
+            m = m % k;
+
+            atePeanut = p / j;
+            p = p % j;
+
+            n = n - ateBanana - atePeanut;
+
+            if (m != 0 || p != 0) {
+                n = n - 1;
+            }
+
+            System.out.println("Number of Monkeys left on the Tree:" + n);
+        }
+
+        sc.close();
+    }
+}
