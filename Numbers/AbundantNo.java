99fc86c5be8a791a332780a5e7133e8456efb1e9
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-03-14 22:27:15 +0530

    Done with the abundant number

---
+++ Numbers/AbundantNo.java
@@ -0,0 +1,31 @@
+package Numbers;
+
+import java.util.*;
+
+public class AbundantNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                if (n / i == 0) {
+                    sum += i;
+                } else {
+                    sum += i;
+                    sum += n / i;
+                }
+            }
+        }
+        sum -= n;
+
+        if (sum > n) {
+            System.out.println("Abundant Number");
+        } else {
+            System.out.println("Not a Abundant Number");
+        }
+        sc.close();
+    }
+}
